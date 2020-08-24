4a74a0a837995cb330fd9baf3f229400c9589a96
Bruce Willke <bruce@brucewillke.com>
2020-08-24 13:36:12 -0400

    Updates

--- build_push_remove_start.sh
+++ build_push_remove_start.sh
@@ -4,7 +4,7 @@ set -e
 TAG=$1
 
 echo "#### Building airconnect ${TAG} ####"
-docker build -t airconnect:${TAG} . &&
+docker build -t airconnect:${TAG} . --no-cache &&
 
 echo "### Tagging airconnect ${TAG} ####"
 docker image tag airconnect:${TAG} brucewillke/airconnect:${TAG} &&
@@ -16,7 +16,8 @@ echo "#### Removing running airconnect container ####"
 docker rm -f airconnect &&
 
 echo "#### Creating new container to run ####"
-sh ../docker-scripts/airconnect.sh &&
+##sh ../docker-scripts/airconnect.sh &&
+docker create --net="host" --name="airconnect" --restart="always" brucewillke/airconnect:latest &&
 
 echo "#### Starting airconnect container ####"
 docker start airconnect
